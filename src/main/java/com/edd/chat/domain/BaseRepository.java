bc6e87fcb478fbfc24b536fc52b5271de4ee9d67
Edvinas <edvinas108@gmail.com>
2016-10-03 22:09:25 +0300

    Remove oauth2 stuff

--- src/main/java/com/edd/chat/domain/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edd.chat.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
-}
--- src/main/java/com/edd/chat/domain/user/ChatUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edd.chat.domain.user;
-
-import com.edd.chat.domain.BaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatUserRepository extends BaseRepository<ChatUser> {
-}
